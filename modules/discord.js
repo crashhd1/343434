93dfc966a9384c8994d5e3b229d3237612d26aa7
crashhd1 <159355290+crashhd1@users.noreply.github.com>
2024-08-21 21:36:14 +0300

    Add files via upload

---
+++ modules/discord.js
@@ -0,0 +1,48 @@
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const EventEmiter = require('events');
+const config = require('../data/config.json');
+
+class ExtendedClient extends Client {
+  constructor() {
+    super({ intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] });
+
+    this.on("ready", () => {
+      // do some post login stuff
+      console.log(`Logged in as ${this.user.username}!`);
+      this.#init();
+    });
+    
+    this.login(config.discord.token);
+
+    this.commands = new EventEmiter();
+  }
+
+  #init() {
+    // create command listeners
+    this.on("message", msg => {
+      if (msg.author.bot) return;
+      
+      // check if message is a command
+      if (!msg.content.startsWith(config.discord.prefix)) return;
+
+      // now fetch the command
+      let parts = msg.content.substring(config.discord.prefix.length).split(" ");
+      let command = parts.splice(0, 1)[0];
+
+      // emit command event
+      this.commands.emit(command, msg, parts);
+
+      // show help command as status
+      this.user.setPresence({ activities: [{ name: `${config.discord.prefix}help`, type: 'LISTENING'}], status: 'online'});
+    });
+  }
+
+  createEmbed(title, description, color = 0xFFFFFF) {
+    return new MessageEmbed()
+      .setTitle(title)
+      .setDescription(description)
+      .setColor(color);
+  }
+}
+
+module.exports = ExtendedClient;
